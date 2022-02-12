69a7b2c14fe711fa5d9f776dcd653864b323b216
Adam Roderick <23650+aroder@users.noreply.github.com>
2022-02-11 17:11:27 -0700

    fix: version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'ply>=3.11', 'pygraphviz>=1.6']
+requirements = ['Click>=7.0', 'ply>=3.11', 'pygraphviz>=1.6'] 
 
 setup_requirements = [ ]
 
